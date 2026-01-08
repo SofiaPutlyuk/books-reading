79e66dac104e1d7debd480e2bd0e3214869cb112
SofiaPutlyuk <sofiaputlyuk@gmail.com>
2026-01-08 10:54:36 +0200

    fix bags registration

--- src/components/infoPage/InfoPage.tsx
+++ src/components/infoPage/InfoPage.tsx
@@ -1,9 +1,11 @@
-import {useNavigate} from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import arrowInfo from "../../assets/svg/arrowInfo.svg"
 const optionHelpYou: string[] = ["Швидше сформулювати свою ціль і розпочати читати", "Пропорційно розподілити навантаження на кожний день", "Відстежувати особистий успіх"]
 const optionYouCan: string[] = ["Формувати особисту думку незалежну від інших", "Підвищити свої професійні якості опираючись на нові знання", "Стати цікавим співрозмовником"]
 export const InfoPage = () => {
-const navigate = useNavigate()
+    const navigate = useNavigate()
+    const location = useLocation()
+    const isSignUpPage = location.pathname === "/sign-up"
     return (
         <>
             <div className="containerInfo">
@@ -17,17 +19,20 @@ const navigate = useNavigate()
                         </div>
                     ))}
                     <p className="titleOption">Також ви зможете</p>
-                    {optionYouCan.map((text,index) => (
+                    {optionYouCan.map((text, index) => (
                         <div className="containerOption" key={index}>
                             <img src={arrowInfo} alt="arrowInfo" />
                             <p className="textOption">{text}</p>
                         </div>
                     ))}
                 </div>
-                <div className="wrapperButton">
-                    <button className="btnOptionLog" onClick={() => navigate("/sign-up")}>Увійти</button>
-                    <button className="btnOptionSign">Реєстрація</button>
-                </div>
+                {!isSignUpPage && (
+                    <div className="wrapperButton">
+                        <button className="btnOptionLog">Увійти</button>
+                        <button className="btnOptionSign" onClick={() => navigate("/sign-up")}>Реєстрація</button>
+                    </div>
+                )}
+
             </div>
         </>
     )
