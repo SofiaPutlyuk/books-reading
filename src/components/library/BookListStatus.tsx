91b8f3b64bcf1a2df52ef1c29a596a73137b510d
SofiaPutlyuk <sofiaputlyuk@gmail.com>
2026-01-21 12:03:53 +0200

    finish work of drag and drop

---
+++ src/components/library/BookListStatus.tsx
@@ -0,0 +1,35 @@
+import readIcon from "../../assets/svg/readIcon.svg"
+type BookList = {
+    name: string,
+    author: string,
+    publish: number,
+    pageCount: number
+}
+type BookProps = {
+    lists: BookList[]
+}
+export const BookList = ({ lists }: BookProps) => {
+    return (
+        <>
+            <div className="intentionHeader">
+                <span className="headerCell">Назва книги</span>
+                <span className="headerCell">Автор</span>
+                <span className="headerCell">Рік</span>
+                <span className="headerCell">Стор.</span>
+            </div>
+            {lists.map((book, index) => (
+                <div key={index} className="wrapperIntention">
+                    <div className="wrapperTitle">
+                        <img src={readIcon} alt="read-icon" />
+                        <p className="title-intention">{book.name}</p>
+                    </div>
+                    <div className="wrapperDescription">
+                        <span className="subtitle-intention">Автор:</span><span className="info-intention">{book.author}</span>
+                        <span className="subtitle-intention">Рік:</span><span className="info-intention">{book.publish}</span>
+                        <span className="subtitle-intention">Стор.:</span><span className="info-intention">{book.pageCount}</span>
+                    </div>
+                </div>
+            ))}
+        </>
+    )
+}
--- src/components/library/LibraryPageStatus.tsx
+++ src/components/library/LibraryPageStatus.tsx
@@ -1,21 +1,18 @@
 import { DropZone } from "./dragDrop/DropZone"
 import { useAppSelector } from "../../redux/hook"
-import readIcon from "../../assets/svg/readIcon.svg"
+import { BookList } from "./BookListStatus"
 export const LibraryPageStatus = () => {
     const info = useAppSelector((state) => state.book.info)
     const reading = info.filter(b => b.status === "reading")
     const finished = info.filter(b => b.status === "finished")
-    const infoReading = reading.map(r => r)
-    console.log(infoReading)
+    const infoStatusBook = info.map(book => ({ ...book }))
+    console.log(infoStatusBook)
     return (
         <>
             <DropZone title="Прочитано" status="finished" className="mainTitleIntention" />
-            {reading.map(b => <div key={b.name} className="wrapperIntention">  <div className="wrapperTitle">
-                <img src={readIcon} alt="read-icon" />
-                <p className="title-intention">{b.name}</p>
-            </div></div>)}
+            <BookList lists={finished} />
             <DropZone title="Читаю" status="reading" className="mainTitleIntention" />
-            {finished.map(b => <div key={b.name}>{b.name}</div>)}
+            <BookList lists={reading} />
         </>
     )
 }
